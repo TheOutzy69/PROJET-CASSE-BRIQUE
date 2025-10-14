c830b4ce57b0edd311d345739b27586db714b144
Serveaux Tao <taoserveauxx@gmail.com>
2025-10-14 08:59:08 +0200

    Ajout des vies et du score

--- .history/UI_20251014083003.py
+++ .history/UI_20251014083003.py
@@ -6,7 +6,7 @@ Objectif : Créer un systéme fonctionnel pour l'interface utilisateur
 """
 
 import tkinter as tk
-from BOX import BOX
+from BOX import Box
 from Ball import Ball
 from Paddle import Paddle
 
@@ -37,38 +37,30 @@ class App(tk.Tk):
         self.quitButton = tk.Button(text="Quit", command=quit)
         self.quitButton.pack(side='bottom')
 
-    def createLabel(self):
-        
-        self.scoreLabel = tk.Label(text="Score : " + str(self.__score))
-        self.scoreLabel.pack(side="top")
-        
-        self.livesLabel = tk.Label(text="Lives : " + str(self.__lives))
-        self.livesLabel.pack(side="top")
     
-    def update(self):
-        
-        self.scoreLabel.config(text="Score : " + str(self.__score))
-        self.livesLabel.config(text="Lives : " + str(self.__lives))
-        
-
+    
+    
     def playGame(self):
-        
+        #for element in self.winfo_children():
+        #   element.destroy()
+        #self.pack_propagate(0)
         self.playButton.destroy()
         self.settingsButton.destroy()
-        
-        self.createLabel()
 
         self.gamespace = tk.Canvas(self, height=800, width=1200, bg='black')
         self.gamespace.pack()
 
         for j in range(5):
             for i in range(10):
-                Boite = BOX(self.gamespace, 120*i, 50*j)
+                Boite = Box(self.gamespace, 120*i, 50*j)
                 Boite.création()
+                self.__bricks.append(Boite)
         
         boule2 = Ball(self.gamespace, 10, 'white', 1200, 800, 20)
         boule2.création()
         boule2.move()
+        #boule = Ball(self.gamespace, 585, 600)
+        #boule.création()
 
         palet = Paddle(self.gamespace, 550, 750)
         palet.création()
